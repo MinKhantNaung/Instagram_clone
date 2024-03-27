fda63bddd526e9c6013d84e45e3e17babdbee1f2
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2024-03-27 22:20:03 +0630

    Home Component / home view

---
+++ resources/views/livewire/home.blade.php
@@ -0,0 +1,20 @@
+<div class="w-full">
+
+    {{-- Header --}}
+    <header></header>
+
+    {{-- main --}}
+    <main class="grid lg:grid-cols-12 gap-8 md:mt-10">
+
+        <aside class="lg:col-span-8 border overflow-hidden h-[1000px]">
+
+        </aside>
+
+        {{-- suggestions --}}
+        <aside class="lg:col-span-4 border hidden lg:block p-4">
+
+        </aside>
+
+    </main>
+
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Livewire\Home;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', Home::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
