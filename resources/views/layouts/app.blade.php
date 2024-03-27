ff15ed8df6f4d6cbb8756fe046e3ad46a803d91c
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2024-03-27 22:04:22 +0630

    finish sidebar blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,11 +30,8 @@
             </div>
             <div class="drawer-side">
               <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
-              <ul class="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-                <!-- Sidebar content here -->
-                <li><a>Sidebar Item 1</a></li>
-                <li><a>Sidebar Item 2</a></li>
-              </ul>
+
+                @include('layouts.sidebar')
 
             </div>
           </div>
