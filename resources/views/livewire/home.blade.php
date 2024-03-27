c9898d096fd7b511aec52e8cabb5bc8a7006088e
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2024-03-27 22:36:26 +0630

    design home\Stories section

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -8,6 +8,18 @@
 
         <aside class="lg:col-span-8 border overflow-hidden h-[1000px]">
 
+            {{-- Stories --}}
+            <section>
+                <ul class="flex overflow-x-auto items-center gap-2">
+                    @for ($i = 0; $i < 15; $i++)
+                        <li class="flex flex-col justify-center w-20 gap-1 p-2">
+                            <x-avatar story src="https://source.unsplash.com/500x500?face-{{ $i }}" class="w-14 h-14" />
+                            <p class="text-xs font-medium truncate">{{ fake()->name }}</p>
+                        </li>
+                    @endfor
+                </ul>
+            </section>
+
         </aside>
 
         {{-- suggestions --}}
