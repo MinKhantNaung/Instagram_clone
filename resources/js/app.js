b1a260d7c3baa9c9b3355ecd7758585751bf16e5
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2024-03-29 22:14:46 +0630

    install Swiper, set Swiper

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,3 +12,27 @@ Alpine.plugin(intersect)
 window.Alpine = Alpine;
 
 Alpine.start();
+
+// core version + navigation, pagination modules:
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination],
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
--- resources/views/livewire/post/item.blade.php
+++ resources/views/livewire/post/item.blade.php
@@ -28,7 +28,25 @@
     {{-- main --}}
     <main>
         <div class="my-2">
-            <x-video />
+            {{-- Swiper JS --}}
+            <div class="swiper">
+                <!-- Additional required wrapper -->
+                <div class="swiper-wrapper">
+                    <!-- Slides -->
+                    <div class="swiper-slide"><x-video /></div>
+                    <div class="swiper-slide">Slide 2</div>
+                    <div class="swiper-slide">Slide 3</div>
+                </div>
+                <!-- If we need pagination -->
+                <div class="swiper-pagination"></div>
+
+                <!-- If we need navigation buttons -->
+                <div class="swiper-button-prev"></div>
+                <div class="swiper-button-next"></div>
+
+                <!-- If we need scrollbar -->
+                <div class="swiper-scrollbar"></div>
+            </div>
         </div>
     </main>
 
