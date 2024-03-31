a27e5a268e763ae90e3050d75db5ed703b7fb9e6
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2024-03-31 17:16:59 +0630

    add properties and methods for Create Post

--- app/Livewire/Post/Create.php
+++ app/Livewire/Post/Create.php
@@ -2,10 +2,19 @@
 
 namespace App\Livewire\Post;
 
+use Livewire\WithFileUploads;
 use LivewireUI\Modal\ModalComponent;
 
 class Create extends ModalComponent
 {
+    use WithFileUploads;
+
+    public $media = [];
+    public $description;
+    public $location;
+    public $hide_like_view;
+    public $allow_commenting;
+
     // can copy from wire:elements github
     /**
      * Supported: 'sm', 'md', 'lg', 'xl', '2xl', '3xl', '4xl', '5xl', '6xl', '7xl'
@@ -13,6 +22,17 @@ class Create extends ModalComponent
     public static function modalMaxWidth(): string
     {
         return '4xl';
+    }
+
+    public function submit()
+    {
+        dd([
+            $this->media,
+            $this->description,
+            $this->location,
+            $this->hide_like_view,
+            $this->allow_commenting,
+        ]);
     }
 
     public function render()
--- resources/views/livewire/post/create.blade.php
+++ resources/views/livewire/post/create.blade.php
@@ -10,7 +10,7 @@
                 Create new post
             </div>
 
-            <button class="text-blue-500 font-bold">
+            <button @disabled(count($media) == 0) wire:loading.attr='disabled' wire:click.prevent='submit' type="button" class="text-blue-500 font-bold">
                 Share
             </button>
         </div>
@@ -18,39 +18,44 @@
 
     <main class="grid grid-cols-12 gap-3 h-full w-full overflow-hidden">
 
+        {{-- Media --}}
         {{-- left side --}}
         <aside class="col-span-12 lg:col-span-7 m-auto items-center w-full overflow-scroll">
-            <label for="custom-file-input" class="m-auto max-w-fit flex-col flex gap-3 cursor-pointer">
-                <input type="file" multiple accept=".jpg,.jpeg,.png" id="custom-file-input" class="sr-only">
 
-                <span class="m-auto">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="w-14 h-14">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
-                    </svg>
-                </span>
+            @if (count($media) == 0)
+                {{-- Trigger Button --}}
+                <label for="custom-file-input" class="m-auto max-w-fit flex-col flex gap-3 cursor-pointer">
+                    <input wire:model.live='media' type="file" multiple accept=".jpg,.jpeg,.png"
+                        id="custom-file-input" class="sr-only">
 
-                <span class="bg-blue-500 text-white text-sm rounded-lg p-2 px-4">
-                    Upload files from computer
-                </span>
-            </label>
+                    <span class="m-auto">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-14 h-14">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                        </svg>
+                    </span>
 
-            <div
-                class="
-                {{-- flex --}}
-                hidden
-                overflow-x-scroll w-[500px] h-96 snap-x snap-mandatory gap-2 px-2">
-                <div class="w-full h-full shrink-0 snap-always snap-center">
-
-                    <x-video />
+                    <span class="bg-blue-500 text-white text-sm rounded-lg p-2 px-4">
+                        Upload files from computer
+                    </span>
+                </label>
+            @else
+                {{-- Show if file count is > 0 --}}
+                <div class="flex overflow-x-scroll w-[500px] h-96 snap-x snap-mandatory gap-2 px-2">
+                    @foreach ($media as $index => $file)
+                        <div class="w-full h-full shrink-0 snap-always snap-center">
+                            @if (strpos($file->getMimeType(), 'image') !== false)
+                                <img src="{{ $file->temporaryUrl() }}" alt=""
+                                    class="w-full h-full object-contain">
+                            @elseif (strpos($file->getMimeType(), 'video') !== false)
+                                <x-video :source="$file->temporaryUrl()" />
+                            @endif
+                        </div>
+                    @endforeach
                 </div>
+            @endif
 
-                <div class="w-full h-full shrink-0 snap-always snap-center">
-                    <img src="https://images.pexels.com/photos/7381200/pexels-photo-7381200.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                        alt="" class="w-full h-full object-contain">
-                </div>
-            </div>
         </aside>
 
         {{-- right side --}}
@@ -61,13 +66,13 @@
             </div>
 
             <div>
-                <textarea placeholder="Add a caption"
+                <textarea wire:model='description' placeholder="Add a caption"
                     class="border-0 focus:border-0 px-0 w-full rounded-lg bg-white h-32 focus:outline-none focus:ring-0" name=""
                     id="" cols="30" rows="10"></textarea>
             </div>
 
             <div class="w-full items-center">
-                <input type="text" placeholder="Add location"
+                <input wire:model='location' type="text" placeholder="Add location"
                     class="border-0 focus:border-0 px-0 w-full rounded-lg bg-white  focus:outline-none focus:ring-0">
             </div>
 
@@ -80,7 +85,7 @@
                             <span>Hide like and view counts on this post</span>
 
                             <label class="relative inline-flex items-center mb-5 cursor-pointer">
-                                <input type="checkbox" value="" class="sr-only peer">
+                                <input wire:model='hide_like_view' type="checkbox" value="" class="sr-only peer">
                                 <div
                                     class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
                                 </div>
@@ -92,7 +97,8 @@
                             <span>Turn off commenting</span>
 
                             <label class="relative inline-flex items-center mb-5 cursor-pointer">
-                                <input type="checkbox" value="" class="sr-only peer">
+                                <input wire:model='allow_commenting' type="checkbox" value=""
+                                    class="sr-only peer">
                                 <div
                                     class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
                                 </div>
