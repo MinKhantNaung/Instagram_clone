f9e64978c336b28b6c06c71ec031493d45204211
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2024-03-31 13:47:04 +0630

    set require wire:elements modal

--- app/Livewire/Post/Create.php
+++ app/Livewire/Post/Create.php
@@ -6,6 +6,15 @@ use LivewireUI\Modal\ModalComponent;
 
 class Create extends ModalComponent
 {
+    // can copy from wire:elements github
+    /**
+     * Supported: 'sm', 'md', 'lg', 'xl', '2xl', '3xl', '4xl', '5xl', '6xl', '7xl'
+     */
+    public static function modalMaxWidth(): string
+    {
+        return '4xl';
+    }
+
     public function render()
     {
         return view('livewire.post.create');
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,26 @@ import forms from '@tailwindcss/forms';
 export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/wire-elements/modal/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+    ],
+
+    safelist: [
+        'sm:max-w-sm',
+        'sm:max-w-md',
+
+        'md:max-w-lg',
+        'md:max-w-xl',
+
+        'lg:max-w-2xl',
+        'lg:max-w-3xl',
+
+        'xl:max-w-4xl',
+        'xl:max-w-5xl',
+
+        '2xl:max-w-6xl',
+        '2xl:max-w-7xl',
     ],
 
     theme: {
