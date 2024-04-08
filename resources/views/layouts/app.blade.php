6394378718a05df76a7e89dfe3f85bafb034ca55
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2024-04-08 15:29:05 +0630

    create livewire Sidebar Component

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,7 +31,8 @@
             <div class="drawer-side">
               <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
 
-                @include('layouts.sidebar')
+                {{-- @include('layouts.sidebar') --}}
+                <livewire:components.sidebar />
 
             </div>
           </div>
